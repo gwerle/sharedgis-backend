499e45dee9371609f252895cdd0ccf197f78382d
Gustavo Werle <gustavo.werle2@gmail.com>
2021-01-26 02:05:47 -0300

    feat: add Roads entity and table

---
+++ src/database/migrations/1611634391679-CreateRoads.ts
@@ -0,0 +1,66 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateRoads1611634391679 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'roads',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'way',
+            type: 'enum',
+            enum: ['ONE_WAY', 'BOTH_WAY'],
+          },
+          {
+            name: 'slope',
+            type: 'enum',
+            enum: ['LOW', 'MEDIUM', 'HIGH'],
+          },
+          {
+            name: 'paved',
+            type: 'boolean',
+          },
+          {
+            name: 'road_condition',
+            type: 'enum',
+            enum: ['EXCELENT', 'GOOD', 'INTERMEDIATE', 'HORRIBLE'],
+          },
+          {
+            name: 'have_bus_lines',
+            type: 'boolean',
+          },
+          {
+            name: 'geom',
+            type: 'geography',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('roads');
+  }
+}
---
+++ src/models/Roads.ts
@@ -0,0 +1,59 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+
+import { Geometry } from 'geojson';
+import Map from './Map';
+
+type RoadWay = 'ONE_WAY' | 'BOTH_WAY';
+type Slope = 'LOW' | 'MEDIUM' | 'HIGH';
+type RoadCondition = 'EXCELENT' | 'GOOD' | 'INTERMEDIATE' | 'HORRIBLE';
+
+@Entity('roads')
+class Roads {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @ManyToOne(() => Map)
+  @JoinColumn({ name: 'id' })
+  map: Map;
+
+  @Column({ name: 'way', type: 'enum', enum: ['ONE_WAY', 'BOTH_WAY'] })
+  way: RoadWay;
+
+  @Column({ name: 'slope', type: 'enum', enum: ['LOW', 'MEDIUM', 'HIGH'] })
+  slope: Slope;
+
+  @Column()
+  paved: boolean;
+
+  @Column({
+    name: 'road_condition',
+    type: 'enum',
+    enum: ['EXCELENT', 'GOOD', 'INTERMEDIATE', 'HORRIBLE'],
+  })
+  road_condition: RoadCondition;
+
+  @Column()
+  have_bus_lines: boolean;
+
+  @Column({ type: 'geography', srid: 4326 })
+  geom: Geometry;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default Roads;
