76614a9fb4cd6f986c12b0544df71f0b82f980e7
Gustavo Werle <gustavo.werle2@gmail.com>
2021-02-01 02:05:04 -0300

    feat: insert GET request for accessibility_ramps

--- src/routes/accessibilityRamps.routes.ts
+++ src/routes/accessibilityRamps.routes.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express';
 import AccessibilityRampService from '../services/AccessibilityRampService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const accessibilityRampsRoutes = Router();
+
+accessibilityRampsRoutes.use(ensureAuthenticated);
 
 accessibilityRampsRoutes.post('/', async (request, response) => {
   const {
@@ -19,6 +22,18 @@ accessibilityRampsRoutes.post('/', async (request, response) => {
     haveVisionNotification,
     lat,
     long,
+  });
+
+  return response.json(accessibilityRamp);
+});
+
+accessibilityRampsRoutes.get('/', async (request, response) => {
+  const { map_id } = request.body;
+
+  const accessibilityRampService = new AccessibilityRampService();
+
+  const accessibilityRamp = await accessibilityRampService.getByMap({
+    map_id,
   });
 
   return response.json(accessibilityRamp);
--- src/services/AccessibilityRampService.ts
+++ src/services/AccessibilityRampService.ts
@@ -1,12 +1,16 @@
 import { getRepository } from 'typeorm';
 import AccessibilityRamp from '../models/AccessibilityRamp';
 
-interface Request {
+interface POSTRequest {
   map_id: string;
   inclination: string;
   haveVisionNotification: boolean;
   lat: number;
   long: number;
+}
+
+interface GETRequest {
+  map_id: string;
 }
 
 class CreateAccessibilityRamp {
@@ -16,13 +20,24 @@ class CreateAccessibilityRamp {
     haveVisionNotification,
     lat,
     long,
-  }: Request): Promise<AccessibilityRamp> {
+  }: POSTRequest): Promise<AccessibilityRamp> {
     const accessibilityRampRepository = getRepository(AccessibilityRamp);
 
     const accesibilityRamp = await accessibilityRampRepository.query(
       'INSERT INTO accessibility_ramps (map_id, inclination, have_vision_notification, geom)' +
         'VALUES ($1, $2, $3, ST_SetSRID(ST_MakePoint($4, $5), 4326))',
       [map_id, inclination, haveVisionNotification, long, lat],
+    );
+
+    return accesibilityRamp;
+  }
+
+  public async getByMap({ map_id }: GETRequest): Promise<AccessibilityRamp> {
+    const accessibilityRampRepository = getRepository(AccessibilityRamp);
+
+    const accesibilityRamp = await accessibilityRampRepository.query(
+      'SELECT id, map_id, inclination, have_vision_notification, ST_X(geom::geometry), ST_Y(geom::geometry) FROM accessibility_ramps WHERE map_id = $1',
+      [map_id],
     );
 
     return accesibilityRamp;
